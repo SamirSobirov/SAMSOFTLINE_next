0de15de855c39a41be0c73a0a0cd36116a1302a2
Sobirov S <sobiroff23@gmail.com>
2024-08-12 21:35:22 +0500

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
 import Image from "next/image";
 
-export default function Page() {
-  return <h1 className="items-center text-center">Hello, Next.js!</h1>
+export default function Home() {
+  return (
+    <header>
+    <nav className="h-[80px] bg-blue-500 w-full items-start justify-center py-[20px]">
+      <ul className="flex px-[50px] gap-12">
+        <li className="">
+          <a href="#services" className="text-white font-semibold">Главная</a>
+        </li>
+        <li className="">
+          <a href="#services" className="text-white font-semibold">О нас</a>
+        </li>
+        <li className="nav-item">
+          <a href="#benefits" className="text-white font-semibold">Плюсы</a>
+        </li>
+        <li className="nav-item">
+          <a href="#adres" className="text-white font-semibold">Наш адрес</a>
+        </li>
+        <li className="nav-item">
+          <a href="#contacts" className="text-white font-semibold">Контакты</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  );
 }
